56bb711d495fe0720aa6b2180c763b060c5df2e7
keerthanvenkat <keerthangkce@gmail.com>
2020-02-28 16:32:35 +0530

    resolved callback issue

--- src/project_local/project_static/client_regi.js
+++ src/project_local/project_static/client_regi.js
@@ -16,38 +16,53 @@ function getsession() {
         return null;
 }
 
-
-function apiRequest(request,callback) {
-    url = '/ContactForm';
-    var sessionToken = getSessionToken();
-    var requestFrame = {
-        'session_token': sessionToken,
-        'request': request
-    };
-
-    var actula_data = toJSON(requestFrame);
-    $.ajax(
-    {
-        type:"POST",
-        contentType: 'application/json',
-        url: url,
-        data:toJSON(actula_data),
-        success: function( data ) 
-        {
-            data = parseJSON(data)
-            var status = data[0];
-            var response = data[1];
-            if (Object.keys(response).length == 0)
-                    callback(status, null);
-            else
-                callback(status, response);
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-            errordata = rdata;
-            callback(errordata, errorThrown);
-        }
-     })
+function getUserInfo() {
+    var info = window.sessionStorage.userInfo;
+    if (typeof info === 'undefined') {
+        user = null;
+    } else {
+        user = JSON.parse(info);
+    }
+    return user;
 }
+function getSessionToken() {
+    var info = getUserInfo();
+    if (info !== null)
+        return info.session_token;
+    else
+        return null;
+}
+// function apiRequest(request,callback) {
+//     url = '/ContactForm';
+//     var sessionToken = getSessionToken();
+//     var requestFrame = {
+//         'session_token': sessionToken,
+//         'request': request
+//     };
+
+//     var actula_data = toJSON(requestFrame);
+//     $.ajax(
+//     {
+//         type:"POST",
+//         contentType: 'application/json',
+//         url: url,
+//         data:toJSON(actula_data),
+//         success: function( data ) 
+//         {
+//             data = parseJSON(data)
+//             var status = data[0];
+//             var response = data[1];
+//             if (Object.keys(response).length == 0)
+//                     callback(status, null);
+//             else
+//                 callback(status, response);
+//         },
+//         error: function(jqXHR, textStatus, errorThrown) {
+//             errordata = rdata;
+//             callback(errordata, errorThrown);
+//         }
+//      })
+// }
 
 
 $(document).ready(function() {
@@ -64,6 +79,36 @@ $(document).ready(function() {
     console.log(ph_no)
     console.log(regards)
     console.log(post_data)
-    apiRequest(post_data,callback)
+    // apiRequest(post_data,callback)
+    url = '/ContactForm';
+    var sessionToken = getSessionToken();
+    var requestFrame = {
+        'session_token': sessionToken,
+        'request': post_data
+    };
+
+    var actula_data = requestFrame;
+    $.ajax(
+    {
+        type:"POST",
+        contentType: 'application/json',
+        url: url,
+        dataType: 'json',
+        data:actula_data,
+        success: function( data ) 
+        {
+            data = data
+            var status = data[0];
+            var response = data[1];
+            if (Object.keys(response).length == 0)
+                    callback(status, null);
+            else
+                callback(status, response);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            errordata = rdata;
+            callback(errordata, errorThrown);
+        }
+     })
     });
 });
